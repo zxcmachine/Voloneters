2309457b8573f498b71b4dca73543f5cb0d5eb73
EvgenyChurilov <85702526+zxcmachine@users.noreply.github.com>
2023-12-15 02:07:18 +0500

    task/bash-23 Рефактор страниц (#19)

--- assets/styles/header.js
+++ assets/styles/header.js
@@ -6,7 +6,6 @@ const regOrgPopup = document.querySelector(".register-org-popup")
 const resetPassPopup = document.querySelector(".reset-pass-popup")
 const newPassPopup = document.querySelector(".new-pass-popup")
 
-console.log(authPopup)
 function openAuthPopup() {
   closeAllHeaderPopups();
   authPopup.classList.add("some-popup_active")
@@ -39,4 +38,27 @@ function closeAllHeaderPopups() {
   regOrgPopup.classList.remove("some-popup_active");
   resetPassPopup.classList.remove("some-popup_active");
   newPassPopup.classList.remove("some-popup_active");
+  if(document.querySelector('.history_score_no_empty')){
+    document.querySelector('.history_score_no_empty').classList.remove('some-popup_active')
+  }
+
+  document.querySelector('body').style.overflow = "unset"
 }
+function openUnversalFilter(el){
+  el.querySelector('.universal__filter-items').classList.toggle('active')
+  el.classList.toggle('active')
+ }
+
+ function openHistoryScore(){
+  document.querySelector('.history_score_no_empty').classList.add('some-popup_active')
+ }
+
+ function history_score_how_to_balance(){
+  document.querySelector('.history_score_how_to_balance').classList.add('some-popup_active')
+ }
+
+ function history_score_how_to_rating(){
+  document.querySelector('.history_score_how_to_rating').classList.add('some-popup_active')
+ }
+
+ 
