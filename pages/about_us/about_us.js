ac6f110b35cf32a688f52489b15b5beb3b7f5ba2
Rrodya <rodionsome@gmail.com>
2023-12-20 00:42:51 +0500

    task/BASH-16: Правки

--- pages/about_us/about_us.js
+++ pages/about_us/about_us.js
@@ -68,3 +68,72 @@ function openStaffListItemPopup() {
   document.querySelector('#staff-item').classList.add('some-popup_active')
   document.querySelector('body').style.overflow = 'unset'
 }
+
+
+new Swiper('.running-list__list-swiper', {
+  loop: true,
+  freeMode: true,
+  direction: 'horizontal',
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: true,
+  },
+  speed: 1000,
+  slidesPerView: 5
+});
+
+const runLineReverse = document.querySelector(".running-line__list-revert");
+const runLineItemsReverse = document.querySelectorAll(".running-line__list-revert-item");
+
+
+runLineReverse.innerHTML = ""
+
+for (let i = 0; i < 3; i ++) {
+  runLineItemsReverse.forEach((item, index) => {
+    if (index !== 0) {
+      runLineReverse.prepend(item.cloneNode(true));
+    }
+    runLineReverse.appendChild(item.cloneNode(true));
+
+  })
+}
+
+const runLine = document.querySelector(".running-list__list-norm");
+const runLineItem = document.querySelectorAll(".running-list__list-norm .running-line__list-item")
+
+runLine.innerHTML = ""
+
+for (let i = 0; i < 6; i ++) {
+  runLineItem.forEach((item, index) => {
+    runLine.prepend(item.cloneNode(true));
+
+  })
+}
+
+const runLineReverseC = document.querySelector(".running-line__list-revert_company");
+const runLineItemsReverseC = document.querySelectorAll(".running-line__list-revert-item_company");
+
+
+runLineReverseC.innerHTML = ""
+
+for (let i = 0; i < 3; i ++) {
+  runLineItemsReverseC.forEach((item, index) => {
+    if (index !== 0) {
+      runLineReverseC.prepend(item.cloneNode(true));
+    }
+    runLineReverseC.appendChild(item.cloneNode(true));
+
+  })
+}
+
+const runLineC = document.querySelector(".running-list__list-norm_company");
+const runLineItemC = document.querySelectorAll(".running-list__list-norm_company .running-line__list-item")
+
+runLineC.innerHTML = ""
+
+for (let i = 0; i < 3; i ++) {
+  runLineItemC.forEach((item, index) => {
+    runLineC.prepend(item.cloneNode(true));
+
+  })
+}
